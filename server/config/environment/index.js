12f7f864e6f25451d683f7accde0407f56c79b55
David George <david.george@uk.ibm.com>
2015-08-05 14:02:40 +0000

    Adding defaults for nlc creds

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -36,7 +36,18 @@ var all = {
   // Default values for VCAP, to be used with node cfenv if not present
   vcap : {
     application : null,
-    services : null
+    services : {
+      'natural_language_classifier': [{
+        'name': 'ibmwatson-nlc-classifier',
+        'label': 'natural_language_classifier',
+        'plan': 'beta',
+        'credentials': {
+          'url': 'https://gateway.watsonplatform.net/natural-language-classifier-beta/api',
+          'username': 'fd323271-b313-440e-9ac5-29f5ed8adf91',
+          'password': 'c62de7383b79'
+        }
+      }]
+    }
   },
 
   endpoints : {
