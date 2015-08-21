651f7a86987481034c67f6b69414a1fc78e9c499
David George <david.george@uk.ibm.com>
2015-08-21 16:42:00 +0100

    Removed imagemin and svgmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -327,29 +327,6 @@ module.exports = function(grunt) {
       }
     },
 
-    // The following *-min tasks produce minified files in the dist folder
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.client %>/assets/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
-          dest: '<%= yeoman.dist %>/public/assets/images'
-        }]
-      }
-    },
-
-    svgmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.client %>/assets/images',
-          src: '{,*/}*.svg',
-          dest: '<%= yeoman.dist %>/public/assets/images'
-        }]
-      }
-    },
-
     // Allow the use of non-minsafe AngularJS files. Automatically makes it
     // minsafe compatible so Uglify does not destroy the ng references
     ngAnnotate: {
@@ -469,9 +446,7 @@ module.exports = function(grunt) {
         }
       },
       dist: [
-        'sass',
-        'imagemin',
-        'svgmin'
+        'sass'
       ]
     },
 
